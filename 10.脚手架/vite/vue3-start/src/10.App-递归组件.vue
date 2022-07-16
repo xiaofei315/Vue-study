d92f56314bd8780b137fc1c4529dfc2034600fbe
zxf <charlie_fly315@163.com>
2022-07-16 23:43:46 +0800

    递归组件

---
+++ 10.脚手架/vite/vue3-start/src/10.App-递归组件.vue
@@ -0,0 +1,53 @@
+<template>
+  <List @onClick="getItem" :data="list"></List>
+</template>
+<script setup lang="ts">
+import {reactive, computed, ref, watch, watchEffect} from 'vue'
+import List from './components/List.vue'
+
+type TreeList = {
+  name: string,
+  children?: TreeList[] | []
+}
+let list = reactive<TreeList[]>([
+  {
+    name: 'no.1',
+    children: [
+      {
+        name: "no.1-1",
+        children: [
+          {name: 'no.1-1-1'},
+          {name: 'no.1-1-2'},
+          {name: 'no.1-1-3'}
+        ]
+      },
+      {
+        name: "no.1-2",
+        children: [
+          {name: 'no.1-2-1'},
+          {name: 'no.1-2-2'},
+          {name: 'no.1-2-3'}
+        ]
+      }
+    ]
+  },
+  {
+    name: 'no.2',
+    children: [
+      {name: 'no.2-1'}
+    ]
+  },
+  {
+    name: 'no.3'
+  }
+])
+const getItem = (item: TreeList) => {
+  console.log(item)
+}
+</script>
+<style scoped>
+html, body, #app {
+  height: 100%;
+  overflow: hidden;
+}
+</style>
