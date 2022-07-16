a3bb9518260853663c462ecad2dbd5b84d090046
zxf <charlie_fly315@163.com>
2022-07-16 23:43:20 +0800

    less

---
+++ 10.脚手架/vite/vue3-start/src/8.App-less.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {reactive, computed, ref, watch, watchEffect} from 'vue'
+import layout from './layout/index.vue'
+
+let show = ref<boolean>(true)
+</script>
+
+<template>
+  <layout></layout>
+</template>
+
+<style scoped>
+
+</style>
