2003b5a029dd98dc6a3633587a78cce715f4e005
zxf <charlie_fly315@163.com>
2022-07-16 23:42:40 +0800

    watchEffect

---
+++ 10.脚手架/vite/vue3-start/src/6.App-watchEffect.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {reactive, computed, ref, watch, watchEffect} from 'vue'
+
+let message = ref<string>("飞机");
+const stop = watchEffect((onInvalidate) => {
+  console.log(message.value)
+  onInvalidate(() => {
+    console.log("before")
+  })
+}, {
+  flush: 'post'
+})
+</script>
+
+<template>
+  <div style="width: 800px;">
+    <input v-model="message" type="text"/>
+    <button @click="stop">停止监听</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
