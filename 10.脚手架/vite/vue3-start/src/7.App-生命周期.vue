6043ea74d09a617f9d01c0636c544ee86dd9ddb4
zxf <charlie_fly315@163.com>
2022-07-16 23:43:06 +0800

    生命周期

---
+++ 10.脚手架/vite/vue3-start/src/7.App-生命周期.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {reactive, computed, ref, watch, watchEffect} from 'vue'
+import hello from './components/Hello.vue'
+
+let show = ref<boolean>(true)
+</script>
+
+<template>
+  <div>
+    <hello v-if="show"></hello>
+    <button @click="show = !show">卸载</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
