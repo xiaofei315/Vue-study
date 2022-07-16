a167956ac6f287ed1808f7a888fc913f198c3cec
zxf <charlie_fly315@163.com>
2022-07-16 23:43:29 +0800

    less

---
+++ 10.脚手架/vite/vue3-start/src/9.App-父子组件通信.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {reactive, computed, ref, watch, watchEffect} from 'vue'
+import layout from './layout/index.vue'
+
+let show = ref<boolean>(true)
+</script>
+
+<template>
+  <layout></layout>
+</template>
+
+<style scoped>
+html,body,#app{
+  height:100%;
+  overflow: hidden;
+}
+</style>
