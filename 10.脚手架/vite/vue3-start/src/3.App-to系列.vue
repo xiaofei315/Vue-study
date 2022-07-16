9dcaee273ae53b4c9c0993f576415365cb3e16bc
zxf <charlie_fly315@163.com>
2022-07-16 23:41:39 +0800

    toRef/toRefs/toRaw

---
+++ 10.脚手架/vite/vue3-start/src/3.App-to系列.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import {toRef, toRefs, toRaw, reactive} from 'vue'
+
+// const obj = {
+//   a: 1,
+//   b: 2
+// }
+// const state1 = toRef(obj, 'a')
+// const change = () => {
+//   state1.value++
+//   console.log("原始对象", obj)
+//   console.log("改变后的对象", state1)
+// }
+
+// const obj2 = reactive({
+//   a: 1,
+//   b: 2
+// })
+// const state2 = toRef(obj2, 'a')
+// const change2 = () => {
+//   state2.value++
+//   console.log("原始对象", obj)
+//   console.log("改变后的对象", state2)
+// }
+
+// const obj3 = reactive({
+//   a: 1,
+//   b: 2
+// })
+// const {a, b} = toRefs(obj3)
+// const change3 = () => {
+//   a.value++
+//   b.value++
+// }
+
+const obj4 = reactive({
+  a: 1,
+  b: 2
+})
+const raw = toRaw(obj4)
+console.log(obj4)
+console.log(raw)
+</script>
+
+<template>
+  <!--  <div>{{ state1 }}</div>-->
+  <!--  <button @click="change">改变</button>-->
+
+  <!--  <div>{{ state2 }}</div>-->
+  <!--  <button @click="change2">改变</button>-->
+
+  <!--  <div>{{ a }}</div>-->
+  <!--  <div>{{ b }}</div>-->
+  <!--  <button @click="change3">改变</button>-->
+</template>
+
+<style scoped>
+
+</style>
