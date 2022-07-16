b62804701be30d0e70ee60ee30779f934ecb249b
zxf <charlie_fly315@163.com>
2022-07-16 23:41:15 +0800

    reactive

---
+++ 10.脚手架/vite/vue3-start/src/2.App-reactive.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {reactive,readonly,shallowReactive} from 'vue'
+
+let abc=shallowReactive({
+  name:'xiaofei' ,//第一层响应式
+  sub:{     // 挂在之前改变可以生效、挂载之后更改，dom不生效
+    sex:'boy'
+  }
+})
+const changeShallow=()=>{
+
+}
+
+let arr = reactive<number[]>([1, 2, 3, 4, 5]) //[]、{}
+let obj = reactive({
+  name: '小飞'
+}) //[]、{}
+setTimeout(() => {
+  arr.push(1);
+  console.log(arr)
+}, 100)
+const changeName = () => {
+  obj.name = '大飞'
+}
+</script>
+
+<template>
+  <div>{{ arr }}</div>
+  <div>{{ obj.name }}</div>
+  <button @click="changeName"></button>
+</template>
+
+<style scoped>
+
+</style>
