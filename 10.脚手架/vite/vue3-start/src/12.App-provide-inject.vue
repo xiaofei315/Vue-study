9fc13d264a031a9dbdc798cdb5ffac9e1aa3b54f
zxf <charlie_fly315@163.com>
2022-07-19 00:03:15 +0800

    组件注入

---
+++ 10.脚手架/vite/vue3-start/src/12.App-provide-inject.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="box">
+    我是根组件
+    <A></A>
+  </div>
+
+</template>
+<script setup lang="ts">
+import {provide} from "vue";
+import A from './components/A.vue'
+
+provide("flag", true)
+</script>
+<style scoped>
+html, body {
+  height: 100%;
+  overflow: hidden;
+}
+
+.box {
+  width: 300px;
+  height: 300px;
+  background: #2c3e50;
+  border: 1px solid #42b983;
+  position: relative;
+}
+</style>
